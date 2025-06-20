8e4f7ecc8d1d3ca27debd25eb43cb827a699aefb
Furfur23 <alfurqon1506@gmail.com>
2025-06-20 18:30:50 +0700

    Cara menggunakan JS

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("HALO DUNIA");
+
+function changeGreeting() {
+    const element = document.getElementById('title');
+
+    console.log(element);
+
+    element.innerText = "Halo Dunia, Saya lagi belajar di Gradient";
+}
