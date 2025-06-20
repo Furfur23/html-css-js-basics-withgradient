a2e7f25c3e0cca2bc208a5ff9699b6a1510ced78
Furfur23 <alfurqon1506@gmail.com>
2025-06-20 18:51:20 +0700

    Membuat elemen secara programatik

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-console.log("HALO DUNIA");
-
 function changeGreeting() {
     const element = document.getElementById('title');
-
-    console.log(element);
-
     element.innerText = "Halo Dunia, Saya lagi belajar di Gradient";
 }
+
+function addCircle() {
+    const container = document.getElementById('circle-container');
+    console.log(container);
+    const circle = document.createElement('div')
+    circle.className = "circle";
+
+    container.appendChild(circle);
+} 
